6e92435f66e8b8078cce6d97f5627f52db52e75b
Sergey <enzzomatrix@gmail.com>
2021-04-28 11:49:59 +1100

    upd

--- MQL4/Libraries/table.cpp
+++ MQL4/Libraries/table.cpp
@@ -4,16 +4,12 @@
 #include <iostream>
 #include <string>
 
+struct some {
+    int x, y;
+};
+
 #define _DLLAPI extern "C" __declspec(dllexport)
 
-_DLLAPI void __stdcall fn() {
-    std::cout << "DEBUG..." << __FILE__;
-    std::ifstream ifs("D://table.csv");
-    if (ifs.is_open()) {
-        std::string line;
-        while (std::getline(ifs, line)) {
-            std::cout << __FILE__ << " " << line << std::endl;;
-        }        
-    }
-    std::system("pause");
+_DLLAPI void __stdcall ParseQuery(some& s) {
+    s = { 1, 2 };
 }
