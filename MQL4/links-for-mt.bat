3e1783604a0268c75f26e682d4a430dc83ed57c7
enzzo <enzzomatrix@gmail.com>
2022-12-03 11:21:50 +1100

    upd

---
+++ MQL4/links-for-mt.bat
@@ -0,0 +1,7 @@
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Experts C:\dev\git\MetaTrader\MQL4\Experts
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Files C:\dev\git\MetaTrader\MQL4\Files
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Include C:\dev\git\MetaTrader\MQL4\Include
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Indicators C:\dev\git\MetaTrader\MQL4\Indicators
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Libraries C:\dev\git\MetaTrader\MQL4\Libraries
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Scripts C:\dev\git\MetaTrader\MQL4\Scripts
+pause
