6711cf8ab978eb71c2f41f43a173765a2c22f691
Sergey <enzzomatrix@gmail.com>
2022-12-18 18:31:43 +1100

    upd

--- MQL4/links-for-mt.bat
+++ MQL4/links-for-mt.bat
@@ -1,6 +1,6 @@
-mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\2AE23EB44C2755DE4A63663BE4CD1AD4\MQL4\Experts D:\dev\git\MetaTrader\MQL4\Experts
-mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\2AE23EB44C2755DE4A63663BE4CD1AD4\MQL4\Include D:\dev\git\MetaTrader\MQL4\Include
-mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\2AE23EB44C2755DE4A63663BE4CD1AD4\MQL4\Indicators D:\dev\git\MetaTrader\MQL4\Indicators
-mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\2AE23EB44C2755DE4A63663BE4CD1AD4\MQL4\Libraries D:\dev\git\MetaTrader\MQL4\Libraries
-mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\2AE23EB44C2755DE4A63663BE4CD1AD4\MQL4\Scripts D:\dev\git\MetaTrader\MQL4\Scripts
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Experts D:\dev\git\MetaTrader\MQL4\Experts
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Include D:\dev\git\MetaTrader\MQL4\Include
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Indicators D:\dev\git\MetaTrader\MQL4\Indicators
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Libraries D:\dev\git\MetaTrader\MQL4\Libraries
+mklink /d C:\Users\Sergey\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Scripts D:\dev\git\MetaTrader\MQL4\Scripts
 pause
